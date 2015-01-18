c9eedb7f7fcb36d9e43779be51588bb56658e1fd
Jenna Le <jenna@oliodigitallabs.com>
2015-01-18 17:17:13 -0500

    Second Draft - Refined Scroll w Icons

--- js/script.js
+++ js/script.js
@@ -14,13 +14,30 @@ $(document).ready(function($) {
 	controller = new ScrollMagic();
 	var splash =  $("#sectionOne");
 	var wipeOut = TweenMax.to(splash,1.5,{opacity:"0"});
-	var sceneOne = new ScrollScene({duration:200});
+	var sceneOne = new ScrollScene({offset:10,duration:200});
 	sceneOne.setTween(wipeOut).addTo(controller);
 	
+	var pin1 = new ScrollScene({triggerElement: "#sectionTwo",offset:0, triggerHook: 0,duration: 500,	reverse: true }).setPin("#sectionTwo .container-fluid"); 
+	var pin2 = new ScrollScene({triggerElement: "#sectionThree",offset: $("#sectionOne .blurb").height()/2,triggerHook: 0, duration: 300,	reverse: true }).setPin("#sectionThree .container-fluid"); 
+	var pin3 = new ScrollScene({triggerElement: "#sectionFour",offset: 0,triggerHook: 0, duration: 500,	reverse: true }).setPin("#sectionFour .container-fluid"); 
+	var pin4 = new ScrollScene({triggerElement: "#sectionFive",offset: 0,triggerHook: 0, duration: 500,	reverse: true }).setPin("#sectionFive .container-fluid"); 
+	
+
+// Add Scenes to ScrollMagic Controller
+controller.addScene([
+  pin1,pin2,pin3,pin4
+]);	
+
+	pin3.addIndicators();
+	pin4.addIndicators();		
+
+
 
 /* --- SECTION 2 --- */
 	var sectionTwo = $("#sectionTwo");
 	var sectionTwoObjects = $("#sectionTwo .headerText, #sectionTwo .blurb");
+	
+	console.log($(window).height()*2);
 	
 	basicScrollIn(sectionTwo,controller,-wHeight/3);	
 		
@@ -29,7 +46,7 @@ $(document).ready(function($) {
 	var sectionThreeObjects = $("#sectionThree .headerText, #sectionThree .blurb");
 
 	basicScrollOut(sectionTwoObjects,sectionThree,controller);
-	basicScrollIn(sectionThree,controller,0);	
+	basicScrollIn(sectionThree,controller,100);	
 	
 	
 	var bgTransition = TweenMax.to("#staticBG",2,{css:{"opacity":"0"}});
@@ -44,7 +61,7 @@ $(document).ready(function($) {
 	var sectionFourObjects = $("#sectionFour .headerText, #sectionFour .blurb");
 	
 	basicScrollOut(sectionThreeObjects,sectionFour,controller);
-	basicScrollIn(sectionFour,controller,0);		
+	basicScrollIn(sectionFour,controller,400);		
 	
 	//Additional Actions (beyond basic actions)
 	var emphasisText = TweenMax.staggerTo("span.emphTxt", 0.5, {color:"#004851", ease:Back.easeIn}, 0.5);	
@@ -69,23 +86,28 @@ $(window).resize(function() {
 });
 
 function basicScrollOut(objects,trigger,controller){
-	var sceneRemovaltween = TweenMax.to(objects,1.5,{css:{opacity:0,top:-300}});
-// 	var sceneRemovaltween = TweenMax.to(objects,1.5,{css:{position:"relative"}});
+
+	var sceneRemovaltween = TweenMax.to(objects,1.5,{css:{opacity:0}});
 	var sceneRemoval = new ScrollScene({triggerElement:trigger,duration:300}).setTween(sceneRemovaltween).addTo(controller);	
+
 }
 	  
 function basicScrollIn(section,controller,offsetVal){
+
+
 	var sectionBlurb = section.find("div.blurb");
 	var sectionHeader = section.find("div.headerText"); 
-
+	var sectionContainer = section.find("div.container-fluid"); 
 	var headerTopPosition = 0;
 	var blurbTopPosition = "30%";
-		
-	var headerEntry = TweenMax.to(sectionHeader,2,{css:{position:"fixed",top:headerTopPosition,left:"0px",right:"0px",opacity:1}});
+
+	var headerEntry = TweenMax.from(sectionHeader,2,{css:{opacity:0}});
 	var sceneHeader = new ScrollScene({triggerElement:section,duration:400,offset:offsetVal}).setTween(headerEntry).addTo(controller);	
 	
-	var blurbEntry = TweenMax.to(sectionBlurb,2,{css:{position:"fixed",top:blurbTopPosition,left:"0px",right:"0px",opacity:1},delay:0});
+	var blurbEntry = TweenMax.from(sectionBlurb,2,{css:{opacity:0}});
 	var sceneBlurb = new ScrollScene({triggerElement:section,duration:400,offset:offsetVal}).setTween(blurbEntry).addTo(controller);	
+
+
 }
 
 function splashResize(){
