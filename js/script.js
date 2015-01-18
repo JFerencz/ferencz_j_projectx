88379364ae912e8f52ad8fbb4e7ce20a181f85b9
Jenna Le <jenna@oliodigitallabs.com>
2015-01-18 02:20:23 -0500

    First Draft

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var wHeight = $(window).height();
 var wWidth = $(window).width();
 
 $(window).load(function() {
-    $("section").height(wHeight-10);
+    $("section").height(wHeight);
 	splashResize();
 });
 
@@ -17,62 +17,89 @@ $(document).ready(function($) {
 	
 
 /* --- SECTION 2 --- */
-	var sectionTwoHeader = $("#sectionTwo .headerText");
-	var sectionTwoBlurb = $("#sectionTwo .blurb");
-	
+	var sectionTwo = $("#sectionTwo");
 	var sectionTwoObjects = $("#sectionTwo .headerText, #sectionTwo .blurb");
 	
-	var headerEntry = TweenMax.to(sectionTwoHeader,1.5,{css:{position:"fixed",top:"50px",left:"0px",right:"0px",opacity:1}});
-	var sceneTwoHeader = new ScrollScene({triggerElement:"#sectionTwo",offset:-wHeight/4,duration:200});	
-	sceneTwoHeader.setTween(headerEntry).addTo(controller);	
-	
-	var blurbEntry = TweenMax.to(sectionTwoBlurb,1.5,{css:{position:"fixed",top:"30%",left:"0px",right:"0px",opacity:1}});
-	var sceneTwoHeader = new ScrollScene({triggerElement:"#sectionTwo",offset:-wHeight/5,duration:200});	
-	sceneTwoHeader.setTween(blurbEntry).addTo(controller);	
-	var sceneTwoBlurb = new ScrollScene({triggerElement:"#sectionTwo",offset:-wHeight/4,duration:200});
-
-	
+	basicScrollIn(sectionTwo,controller);	
 		
 /* --- SECTION 3 --- */
-	var sceneTwoRemovaltween = TweenMax.to(sectionTwoObjects,1.5,{css:{opacity:0,top:-100}});
-	var sceneTwoRemoval = new ScrollScene({triggerElement:"#sectionThree",offset:-wHeight/4,duration:200});	
+	var sectionThree = $("#sectionThree");
+	var sectionThreeObjects = $("#sectionThree .headerText, #sectionThree .blurb");
+
+	basicScrollOut(sectionTwoObjects,sectionThree,controller);
+	basicScrollInVideo(sectionThree,controller);	
 	
-// 	var sceneThreeBG = TweenMax.to("body",0.5,{css:{"background-image":"url(./image/watercolour_bg_2b.jpg)"}})
-	sceneTwoRemoval.setTween(sceneTwoRemovaltween).addTo(controller);	
+	
+// 	var bgTransition = TweenMax.to("body",2,{css:{"background-image":"url(./image/watercolour_bg_2b.jpeg)"}})
+// 	var sceneThreeBG = new ScrollScene({triggerElement:"#sectionThree",offset:-wHeight/4,duration:200}).setTween(bgTransition).addTo(controller);
+	
 	$(window).resize(function() {
 		splashResize();
     });
     
-    
-    
 /* --- SECTION 4 --- */
-	var sectionFourHeader = $("#sectionFour .headerText");
-	var sectionFourBlurb = $("#sectionFour .blurb");
-	
+	var sectionFour = $("#sectionFour");
 	var sectionFourObjects = $("#sectionFour .headerText, #sectionFour .blurb");
 	
-	var headerEntry = TweenMax.to(sectionFourHeader,1.5,{css:{position:"fixed",top:"50px",left:"0px",right:"0px",opacity:1}});
-	var sceneFourHeader = new ScrollScene({triggerElement:"#sectionFour",offset:-wHeight/4,duration:200});	
-	sceneFourHeader.setTween(headerEntry).addTo(controller);	
+	basicScrollOut(sectionThreeObjects,sectionFour,controller);
+	basicScrollIn(sectionFour,controller);		
 	
-	var blurbEntry = TweenMax.to(sectionFourBlurb,1.5,{css:{position:"fixed",top:"30%",left:"0px",right:"0px",opacity:1}});
-	var sceneFourHeader = new ScrollScene({triggerElement:"#sectionFour",offset:-wHeight/5,duration:200});	
-	sceneFourHeader.setTween(blurbEntry).addTo(controller);	
-	var sceneFourBlurb = new ScrollScene({triggerElement:"#sectionFour",offset:-wHeight/4,duration:200});
-
-	var sceneFourEmphText = new ScrollScene({triggerElement:"#sectionFour",offset:-wHeight/4+250});
-	var emphasisText = TweenMax.staggerTo("span.emphTxt", 0.5, {color:"#004851", ease:Back.easeIn}, 0.5);
-	sceneFourEmphText.setTween(emphasisText).addTo(controller);	
+	//Additional Actions (beyond basic actions)
+	var emphasisText = TweenMax.staggerTo("span.emphTxt", 0.5, {color:"#004851", ease:Back.easeIn}, 0.5);	
+	var sceneFourEmphText = new ScrollScene({triggerElement:"#sectionFour",offset:-wHeight/4+250}).setTween(emphasisText).addTo(controller);
 	
-	var sceneFourEmphImg  = new ScrollScene({triggerElement:"#sectionFour",offset:-wHeight/4+250});
 	var emphasisImg = TweenMax.staggerTo(".emph", 0.5, {opacity:"1", ease:Back.easeIn}, 0.5);
-	sceneFourEmphImg.setTween(emphasisImg).addTo(controller);	
-	  
+	var sceneFourEmphImg  = new ScrollScene({triggerElement:"#sectionFour",offset:-wHeight/4+250}).setTween(emphasisImg).addTo(controller);	
 
+/* --- SECTION 5 --- */
+
+	var sectionFive = $("#sectionFive");
+	var sectionFiveObjects = $("#sectionFive .headerText, #sectionFive .blurb");
+
+	basicScrollOut(sectionFourObjects,sectionFive,controller);
+	basicScrollIn(sectionFive,controller);	
+
+
+$(window).resize(function() {
+	splashResize();
+});
 	
 });
+
+function basicScrollOut(objects,trigger,controller){
+	var sceneRemovaltween = TweenMax.to(objects,1.5,{css:{opacity:0,top:-100}});
+	var sceneRemoval = new ScrollScene({triggerElement:trigger,offset:-wHeight/4,duration:200}).setTween(sceneRemovaltween).addTo(controller);	
+}
+	  
+function basicScrollIn(section,controller){
+	var sectionBlurb = section.find("div.blurb");
+	var sectionHeader = section.find("div.headerText"); 
+
+	var headerTopPosition = 50;
+	var blurbTopPosition = $("#sectionTwo .headerText").height() +100;
+		
+	var headerEntry = TweenMax.to(sectionHeader,1.5,{css:{position:"fixed",top:headerTopPosition,left:"0px",right:"0px",opacity:1}});
+	var sceneHeader = new ScrollScene({triggerElement:section,offset:-wHeight/4,duration:200}).setTween(headerEntry).addTo(controller);	
+	
+	var blurbEntry = TweenMax.to(sectionBlurb,1.5,{css:{position:"fixed",top:blurbTopPosition,left:"0px",right:"0px",opacity:1},delay:0});
+	var sceneBlurb = new ScrollScene({triggerElement:section,offset:-wHeight/5,duration:200}).setTween(blurbEntry).addTo(controller);	
+}
+
+function basicScrollInVideo(section,controller){
+	var sectionBlurb = section.find("div.blurb");
+	var sectionHeader = section.find("div.headerText"); 
+
+	var headerTopPosition = 50;
+	var blurbTopPosition = $("#sectionTwo .headerText").height() +100;
+		
+	var headerEntry = TweenMax.to(sectionHeader,1.5,{css:{position:"fixed",top:headerTopPosition,left:"0px",right:"0px",opacity:1}});
+	var sceneHeader = new ScrollScene({triggerElement:section,offset:-wHeight/4,duration:200}).setTween(headerEntry).addTo(controller);	
+	
+	var blurbEntry = TweenMax.to(sectionBlurb,0.5,{css:{position:"fixed",top:blurbTopPosition,left:"0px",right:"0px",opacity:1},delay:0});
+	var sceneBlurb = new ScrollScene({triggerElement:section,offset:-wHeight/4+200}).setTween(blurbEntry).addTo(controller);	
+}
+
 function splashResize(){
 	elementOffset = $(window).height() - $('#splashTitle img').height();
-    console.log(elementOffset);
     $("#splashTitle div").height(elementOffset/2);	
 } 
